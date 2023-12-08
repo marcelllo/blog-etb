81a9becafd43c282f1764c81906e87534d4e18c0
Marcelo Santos <marcello.santos@gmail.com>
2023-12-08 01:15:26 +0000

    JDK 20 + Netbeans 19 added

--- data/data.js
+++ data/data.js
@@ -285,6 +285,12 @@ export default function data() {
           name: "JDK 8 NetBeans 8.2",
           type: "exe",
           url: "http://docs.etb.com.br/s/XHLwmZQwJ6MMrZk"
+        },
+        {
+          id: "5fbfe6d74c7b0000cc007c400103",
+          name: "JDK 20 NetBeans 19",
+          type: "zip",
+          url: "http://docs.etb.com.br/s/Qz6Xg39RNLfpMZS"
         }
       ]
     },
